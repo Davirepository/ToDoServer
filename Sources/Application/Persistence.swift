eae851838ea7bac0a1298028da6554b3b422f7eb
David <bolotaev-david@mail.ru>
2019-08-31 19:17:44 +0300

    Initial Commit

---
+++ Sources/Application/Persistence.swift
@@ -0,0 +1,22 @@
+//
+//  Persistence.swift
+//  Application
+//
+//  Created by Давид on 31/08/2019.
+//
+
+import SwiftKueryORM
+import SwiftKueryPostgreSQL
+
+class Persistence {
+    static func setUp() {
+        let pool = PostgreSQLConnection.createPool(
+            host: "localhost",
+            port: 5432,
+            options: [.databaseName("tododb")],
+            poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50)
+        )
+        
+        Database.default = Database(pool)
+    }
+}
